5563406271e071b731bc13bfc08629107de8f027
Nik <Qzmpqzmp35@yandex.ru>
2024-11-28 18:03:39 +0700

    Удалены объектные файлы из индекса и добавлены в .gitignore

--- primitives/src/main.cpp
+++ primitives/src/main.cpp
@@ -13,13 +13,13 @@ int main(int argc, char const *argv[])
 {
     Timer timer;
 
-    // RunMutex(10,10);
+    //RunMutex(10,10);
     // RunSemaphore(5,10);
-    // RunSemaphoreSlim(50,5);
-    // RunBarrier(10,10);
+     //RunSemaphoreSlim(5,5);
+    //RunBarrier(10,10);
     // RunSpinLock(10,10);
     //RunSpinWait(10,10); // работает плохо
-    // RunMonitor(10, 10);
+    RunMonitor(10, 10);
     return 0;
 }
 
